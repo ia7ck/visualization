fb7067e737893d8f83655d7a8f1c4bfde0ec0946
ia7ck <ikeda.bc.d@gmail.com>
2018-08-31 02:30:58 +0900

    update kruskal

--- kruskal/index.js
+++ kruskal/index.js
@@ -1,122 +1,116 @@
 const n = randInt(10, 15)
 let nodes = new vis.DataSet([...Array(n).keys()].map((i) => ({ id: i })))
-
 let edges = new vis.DataSet()
 let weightedEdges = []
-for (let i = 1; i < n; i++) {
-  const weight = randInt(1, 10)
-  const addedIDs = edges.add({ from: randInt(0, i), to: i, label: (String)(weight) })
-  weightedEdges.push({ id: addedIDs[0], weight })
-}
-for (let _ = 0; _ < Math.ceil(n * 0.7); _++) {
-  const v = randInt(1, n), u = randInt(0, v)
-  let found = false
-  edges.forEach((e) => {
-    if (e.from === u && e.to === v) {
-      found = true
-    }
-  })
-  if (found === false) {
-    const weight = randInt(1, 10)
+
+function build() {
+  for (let i = 1; i < n; i++) {
+    addEdge(randInt(0, i), i, randInt(1, 10))
+  }
+  for (let _ = 0; _ < Math.ceil(n * 0.7); _++) {
+    const v = randInt(1, n), u = randInt(0, v)
+    let found = false
+    edges.forEach((edge) => { found = found || (edge.from === u && edge.to === v) })
+    if (found === false) { addEdge(u, v, randInt(1, 10)) }
+  }
+  function addEdge(u, v, weight) {
     const addedIDs = edges.add({ from: u, to: v, label: (String)(weight) })
-    weightedEdges.push({ id: addedIDs[0], weight })
+    weightedEdges.push({ id: addedIDs[0], weight }) // 1個addしたので、先頭にそのidが入ってる
   }
 }
+function randInt(lb, ub) {
+  return Math.floor(Math.random() * (ub - lb)) + lb;
+}
+
+build()
+
 let data = { nodes, edges }
 const options = {
   nodes: { color: { background: "whitesmoke", border: "lightgray" }, chosen: { node: false }, shape: "dot", size: 10 },
-  edges: { font: { color: "#505050" }, color: { color: "lightgray", inherit: false }, chosen: { edge: false } },
-  physics: { barnesHut: { damping: 0.2 } },
+  edges: { font: { color: "#505050", size: 18 }, color: { color: "lightgray", inherit: false }, chosen: { edge: false } },
+  physics: { enabled: false },
 }
-const container = document.getElementById('main')
+const container = document.getElementById("main")
 const network = new vis.Network(container, data, options)
 
-weightedEdges.sort((l, r) => (l.weight - r.weight))
-let g = new Array(n)
-for (let i = 0; i < n; i++) g[i] = []
-const getCycle = (i, s, t, visited, stID) => {
-  if (i === t) {
-    return [stID]
-  } else {
-    visited[i] = true
-    for (let j = 0; j < g[i].length; j++) {
-      const nxt = g[i][j].to;
-      if (visited[nxt] === false) {
-        const path = getCycle(nxt, s, t, visited, stID)
-        if (path.length > 0) {
-          return path.concat(g[i][j].id)
-        }
-      }
-    }
-    return []
-  }
-}
 const sleep = (ms) => (new Promise(resolve => setTimeout(resolve, ms)))
-const UNIT_TIMEOUT = 500;
-let timeout = UNIT_TIMEOUT;
+const BASE_TIMEOUT = 1000;
+let timeout = BASE_TIMEOUT;
 (async () => {
-  let removalEdgeIDs = []
+  weightedEdges.sort((l, r) => (l.weight - r.weight))
+  let g = new Array(n)
+  for (let i = 0; i < n; i++) g[i] = []
+  // let removalEdgeIDs = []
   for (let i = 0; i < weightedEdges.length; i++) {
-    await sleep(timeout * 3 / 2)
-    const e = edges.get(weightedEdges[i].id)
+    const edge = edges.get(weightedEdges[i].id)
     edges.update({
-      id: e.id,
+      id: edge.id,
       color: { color: "lightblue" },
       width: 2,
     })
-    await sleep(timeout * 2)
+    await sleep(timeout)
     const visited = new Array(n).fill(false)
-    const cycleIDs = getCycle(e.from, e.from, e.to, visited, e.id)
+    const cycleIDs = getCycle(edge.from, edge.from, edge.to, visited, edge.id)
     if (cycleIDs.length === 0) {
-      g[e.from].push({ to: e.to, id: e.id })
-      g[e.to].push({ to: e.from, id: e.id })
+      g[edge.from].push({ to: edge.to, id: edge.id })
+      g[edge.to].push({ to: edge.from, id: edge.id })
       edges.update({
-        id: e.id,
+        id: edge.id,
         color: { color: "skyblue" },
         width: 2,
       })
       nodes.update({
-        id: e.from,
+        id: edge.from,
         color: { background: "lightcyan", border: "skyblue" },
         borderWidth: 1.5,
       })
       nodes.update({
-        id: e.to,
+        id: edge.to,
         color: { background: "lightcyan", border: "skyblue" },
         borderWidth: 1.5,
       })
+      await sleep(timeout)
     } else {
       edges.update(cycleIDs.map((id) => ({ id, color: { color: "lightcoral" } })))
       let nodeSet = new Set()
       cycleIDs.forEach((id) => {
-        const _e = edges.get(id)
-        nodeSet.add(_e.from)
-        nodeSet.add(_e.to)
+        const _edge = edges.get(id)
+        nodeSet.add(_edge.from)
+        nodeSet.add(_edge.to)
       })
       nodes.update([...nodeSet].map((id) => ({ id, color: { background: "mistyrose", border: "lightcoral" } })))
-      await sleep(timeout * 2)
+      await sleep(timeout)
       edges.update(cycleIDs.map((id) => ({ id, color: { color: "skyblue" } })))
       nodes.update([...nodeSet].map((id) => ({ id, color: { background: "lightcyan", border: "skyblue" } })))
-      removalEdgeIDs.push(e.id)
-      edges.update({
-        id: e.id,
-        hidden: true,
-      })
+      edges.remove(edge.id)
+      await sleep(timeout)
     }
   }
-  edges.remove(removalEdgeIDs)
+  function getCycle(i, s, t, visited, stID) {
+    if (i === t) {
+      return [stID]
+    } else {
+      visited[i] = true
+      for (let j = 0; j < g[i].length; j++) {
+        const nxt = g[i][j].to;
+        if (visited[nxt] === false) {
+          const path = getCycle(nxt, s, t, visited, stID)
+          if (path.length > 0) {
+            return path.concat(g[i][j].id)
+          }
+        }
+      }
+      return []
+    }
+  }
 })()
 
+
 document.getElementById("speed").addEventListener("change", (ev) => {
-  if (ev.target.value === "x1") {
-    timeout = UNIT_TIMEOUT
-  } else if (ev.target.value === "x2") {
-    timeout = UNIT_TIMEOUT / 2
-  } else if (ev.target.value === "x4") {
-    timeout = UNIT_TIMEOUT / 4
+  const val = parseInt(ev.target.value, 10)
+  if (Number.isInteger(val)) {
+    if (val === 1 || val === 2 || val === 4) {
+      timeout = BASE_TIMEOUT / val
+    }
   }
 })
-
-function randInt(lb, ub) {
-  return Math.floor(Math.random() * (ub - lb)) + lb;
-}
